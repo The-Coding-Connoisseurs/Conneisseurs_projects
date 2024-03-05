ed31eabcb6a1dbc5f48e0d2e8e6e568a0142d743
Kevin Malekera <r0900654@student.thomasmore.be>
2024-03-05 09:29:36 +0100

    Added animation for lifebuoy and shark fin images

--- src/js/wordLogo/oceanScript.js
+++ src/js/wordLogo/oceanScript.js
@@ -1,28 +1,33 @@
 'use strict';
 
-// Boat element
+// Store the images
 const boat = document.querySelector('.boat');
+const sharkFin = document.querySelector('.shark-fin');
+const lifebuoy = document.querySelector('.lifebuoy');
 
-// Function to animate boat
-function animateBoat() {
-    const currentTime = Date.now();
-    // Value based on the wave animation
-    const waveAmplitude = 25;
-    // Boat's speed
-    const boatSpeed = 0.0015;
+// Function to animate images
+function animateImages() {
+  const currentTime = Date.now();
+  // Adjust the wave animation
+  const waveAmplitude = 50;
+  // Adjust the boat's speed
+  const boatSpeed = 0.0016;
 
-    // Boat's new vertical position
-    const newY = waveAmplitude * Math.sin(boatSpeed * currentTime);
+  // Calculate the vertical position for the images
+  const boatY = waveAmplitude * Math.sin(boatSpeed * currentTime);
+  const sharkFinY = waveAmplitude * Math.sin(boatSpeed * currentTime + 0.5);
+  const lifebuoyY = waveAmplitude * Math.sin(boatSpeed * currentTime - 0.3);
 
-    // New boat position
-    boat.style.bottom = `${50 + newY}px`;
+  // Apply the new positions to the elements
+  boat.style.bottom = `${50 + boatY}px`;
+  sharkFin.style.bottom = `${100 + sharkFinY}px`;
+  lifebuoy.style.bottom = `${100 + lifebuoyY}px`;
 }
 
 // Start animation loop
 function startAnimationLoop() {
-    setInterval(animateBoat, 10);
+    setInterval(animateImages, 10);
 }
 
-// Start animation loop when the page loads
+// Start animation loop when content is loaded
 window.addEventListener('DOMContentLoaded', startAnimationLoop);
-
