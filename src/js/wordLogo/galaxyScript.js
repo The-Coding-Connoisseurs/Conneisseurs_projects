3a259a552502cd59d506ff07943b2ebdd8bf2727
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-03-05 17:21:56 +0100

    FINE TUNED orbits spacing - SOUND looping - GALAXY done :)

--- src/js/wordLogo/galaxyScript.js
+++ src/js/wordLogo/galaxyScript.js
@@ -3,6 +3,7 @@ console.log('[debug] galaxyScript works!');
 document.body.addEventListener('click', playIntro, true); 
 let introSound = new Audio("../../assets/audio/astralChoir.mp3");
 introSound.volume = 0.3;
+introSound.loop = true;
 
 function playIntro() {
     introSound.play();
